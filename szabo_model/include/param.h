bd3c215224ed3ee2c091cb3c3f06ed1f6a829ecf
Carine Beatrici <carine_beatrici@yahoo.com.br>
2022-10-18 12:16:07 -0300

    Add files via upload

--- szabo_model/include/param.h
+++ szabo_model/include/param.h
@@ -1,1 +1,58 @@
+#define TIME_FINAL           220000
+#define SNAPSHOT             2000
+#define L_TENTATIVA          36
 
+#define QUADRADO             0
+#define CIRCULO              1
+#define CANTO                2
+#define EVERYWHERE           3
+
+#define NO                   0
+#define YES                  1
+
+#define KILL                 YES
+#define SORTEIO              CANTO //EVERYWHERE
+
+#define R_MAX                1.00    //1.0  //0.55
+#define R_CORE               0.18    //0.36 //0.2
+#define R_EQ                 0.6666     //0.73 //0.4
+#define DISPERSION           0.1
+
+#define V0                   1.0
+#define V1                   V0
+#define RESEVOIR_DENSITY     2.00 / (0.785398163*R_EQ*R_EQ)  
+#define MU                   1.00
+#define DT                   0.005
+
+#define TAU                  100.0 
+#define F_ADH                1.0
+#define CREATION_PROBABILITY 0.01
+
+#define NOISE                1.0 * 1.414213562
+#define F_0                  30.0
+#define F_REP                30.0
+
+#define GAUSSIAN             YES
+#define WHITE                NO
+
+#define PI                   3.14159265
+#define LOAD                 YES
+
+#define Lx                   105     //275
+#define Ly                   25   //67
+
+#define OBSTACULO            YES
+#define PAREDE               YES
+
+#define L_CANTO              10.0
+#define R_ESFERA             3.75  
+
+#define R_FRONT              -13.0
+
+#define L_CENTRO_X           50.0    //133.333
+#define L_CENTRO_Y           12.5    //34.5
+
+#define N                    50000
+#define N0                   5000
+
+#define MAX_VIZ              20
