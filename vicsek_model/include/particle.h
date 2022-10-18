46171e078f7eccc6f515fa3b24d1fac7f9f14c8e
Carine Beatrici <carine_beatrici@yahoo.com.br>
2022-10-18 11:47:26 -0300

    Add files via upload

---
+++ vicsek_model/include/particle.h
@@ -0,0 +1,32 @@
+typedef struct
+{
+   float x;
+   float y;
+   float v0;
+   float vx;
+   float vy;
+   int box;
+   int next;
+   int type;
+   int exist;
+   int id;
+}particle;
+
+typedef struct
+{
+   int begin;
+   int end;
+   int number;
+}int_3d;
+
+typedef struct
+{
+   int v0;
+   int v1;
+   int v2;
+   int v3;
+   int v4;
+   int v5;
+   int v6;
+   int v7;
+}neighbor_box;
