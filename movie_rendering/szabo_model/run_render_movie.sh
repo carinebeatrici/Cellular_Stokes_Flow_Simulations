fff7be6809a7a19a5e5dea828651f17e09de38ae
Carine Beatrici <carine_beatrici@yahoo.com.br>
2024-03-02 11:39:28 -0300

    Create run_render_movie.sh

---
+++ movie_rendering/szabo_model/run_render_movie.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Creating povray files"
+gfortran povt.f90 -o pov -O3
+./pov
+
+echo "Rendering Images"
+for i in *.pov
+do
+  povray $i +W1334 +H1000 -D 2>/dev/null;
+  echo $i
+done
+
+echo "Rendering Movie"
+for f in ray*.png; do cat "$f"; done | ffmpeg -r 10 -f image2pipe -i - -vcodec libx264 -pix_fmt yuv420p output.avi
